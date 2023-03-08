e4e4fb2e831b408ab5c12039ebdba1122fcdefd1
MartaDorokhova <112471191+MartaDorokhova@users.noreply.github.com>
2023-03-08 14:31:46 +0300

    Add files via upload

---
+++ car/car.js
@@ -0,0 +1,21 @@
+const carName = 'Шустрик';
+let maxCarSpeed = '10 км/ч';
+let carOwner = 'Фантомас';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = '150 км/ч';
+console.log("У машины изменилась скорость");
+console.log(maxCarSpeed);
+carOwner = 'Пушкин';
+console.log("У машины изменился владелец");
+console.log(carOwner);
+
+maxCarSpeed = '1 км/ч';
+console.log("У машины изменилась скорость");
+console.log(maxCarSpeed);
+carOwner = 'крокодил Гена';
+console.log("У машины изменился владелец");
+console.log(carOwner);
---
+++ comment/index.js
@@ -0,0 +1,15 @@
+//JavaScript-код с созданием переменных
+/*Автор
+Дорохова Марта*/
+
+const myName = 'Марта';
+const mySurname = 'Дорохова';
+let myFavoriteDrink = 'Кофе';
+const myFavotiteAnimal = 'кот Кеша';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavotiteAnimal);
+console.log(myFavoriteProgrammingLanguage);
